8b16ec157cdd4294ad20a84467b2c59a37f9877d
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-05 16:39:58 -0500

    Create name_cases.py

---
+++ chapter2/name_cases.py
@@ -0,0 +1,13 @@
+#setting variables for names
+firstName = "andrea"
+lastName = "jimenez"
+person1 = (f"{firstName} {lastName}")
+
+print(person1.lower())
+print()
+print(person1.upper(), "\n")
+print(person1.title())
+print(5 * "\n")
+print(person1.upper())
+
+#I deicded to add new empty lines in different ways just for fun

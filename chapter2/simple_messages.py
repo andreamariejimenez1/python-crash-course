80cda8ebe90cf8cf98f833cddbf169a0311397f2
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-05 16:16:38 -0500

    Create simple_messages.py
    Python keeps track of the current value

---
+++ chapter2/simple_messages.py
@@ -0,0 +1,8 @@
+#assign a message to a variable
+#print the message
+#change the value of that variable and print it
+
+message = "Hello, world!"
+print(message)
+message = "Changing this message now."
+print(message)

fd5815cae6d99e784da41a886a119ca6ca657269
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-05 18:10:45 -0500

    Create names.py

---
+++ chapter3/names.py
@@ -0,0 +1,10 @@
+#GOAL: CREATE A LIST AND PRINT EACH ELEMENT
+
+names = ['Andrea', 'Jesus', 'Dani', 'Janet', 'Flor']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[-1])
+print(names[-2])

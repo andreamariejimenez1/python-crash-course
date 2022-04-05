ea21af33526edbcc73996ce39695f105ef239aef
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-05 18:16:29 -0500

    Create greetings.py

---
+++ chapter3/greetings.py
@@ -0,0 +1,18 @@
+#GOAL: CREATE A LIST AND PRINT EACH ELEMENT WITH A MESSAGE
+
+names = ['Andrea', 'Jesus', 'Dani', 'Janet', 'Flor']
+message = "Hello,"
+
+print(f"{message} {names[0]}!")
+print(f"{message} {names[1]}!")
+print(f"{message} {names[2]}!")
+print(f"{message} {names[3]}!")
+print(f"{message} {names[4]}!")
+
+print()
+
+print(f"Goodbye, {names[0].upper()}!")
+print(f"Goodbye, {names[1].upper()}!")
+print(f"Goodbye, {names[2].upper()}!")
+print(f"Goodbye, {names[3].upper()}!")
+print(f"Goodbye, {names[4].upper()}!")

baf8a4d2fb802d82c2d28e392570e9e0560b86d9
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-05 16:53:32 -0500

    Create famous_quote2.py

---
+++ chapter2/famous_quote2.py
@@ -0,0 +1,13 @@
+#find a quote from a famous person
+#ceate a variable for the famous person
+#compose message and represent it with a new variable
+#print the message 
+
+# print(' Eric Thomas said, "Everybody has a dream, but not everybody has a grind." ')
+
+# message = (f'{famousPerson} "said, Everybody has a dream, but not everybody has a grind."')
+
+famousPerson = "Eric Thomas"
+quote = '"everybody has a dream, but not everybody has a grind"'
+message = (f'{famousPerson} said, {quote}.')
+print(message)

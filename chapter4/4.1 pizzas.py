00e23d3746ed61397b751b158c0d9fcf83e6d84f
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-11 12:46:38 -0500

    Create 4.1 pizzas.py

---
+++ chapter4/4.1 pizzas.py
@@ -0,0 +1,6 @@
+# GOAL: CREATE A LIST OF PIZZAS AND PRINT A STATEMENT FOR EACH. THEN PRINT A LINE OUTSIDE THE LOOP.
+
+pizzas = ['cheese', 'pepperoni', 'supreme']
+for pizza in pizzas:
+  print(f"I don't like {pizza}.")
+print("\nI don't like pizza...")

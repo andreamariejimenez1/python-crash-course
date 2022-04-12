1f0841e03584861da70c045c16a395bed34b55cf
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-12 11:12:03 -0500

    Create 5.1 conditional_test.py

---
+++ chapter5/5.1 conditional_test.py
@@ -0,0 +1,19 @@
+# WRITE A SERIES OF CONDITIONAL TEST
+# PRINT YOUR PREDICTION
+# EQUALITY (==)
+
+name = 'Andrea'
+print("Is name == 'Andrea' ? \nI predict True.")
+print(name == 'Andrea')
+
+print("\nIs name == 'andrea' ? \nI predict False.")
+print(name == 'andrea')
+
+print("\nIs name.title() == 'Andrea' ? \nI predict True.")
+print(name.title() == 'Andrea')
+
+print("\nIs name.upper() == 'Andrea' ? \nI predict false.")
+print(name.upper() == 'Andrea')
+
+print("\nIs name.upper() == 'ANDREA' ? \nI predict True.")
+print(name.upper() == 'ANDREA')

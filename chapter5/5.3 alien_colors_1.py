ca88d4d41ef50a57624306321f586a36a2596158
Andrea Jimenez <100242383+okandreajimenez@users.noreply.github.com>
2022-04-13 18:23:40 -0500

    Create 5.3 alien_colors_1.py

---
+++ chapter5/5.3 alien_colors_1.py
@@ -0,0 +1,9 @@
+#this one fails
+alien_color = "red"
+if alien_color == "green":
+  print("You just earned 5 points.")
+
+#this one passes
+alien_color = "green"
+if alien_color == "green":
+  print("You just earned 5 points.")
